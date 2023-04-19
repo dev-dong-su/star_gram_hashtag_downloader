8856b3e5edd306f73a4d85680d6ae8db3b92465f
Ubuntu <ubuntu@ip-172-31-47-100.ap-northeast-1.compute.internal>
2023-04-19 02:03:15 +0000

    디바이스 정보 변경

--- insta.py
+++ insta.py
@@ -8,7 +8,8 @@ import logging
 import threading
 import requests
 
-row_count = 6278
+row_count = 509
+hashtag_file = 0
 row_count_lock = threading.Lock()
 
 logger = logging.getLogger()
@@ -16,9 +17,13 @@ logger = logging.getLogger()
 
 def login_user(account_index):
     cl = Client()
+    
+    random_device = generate_random_device()
     username = data['account'][account_index]['id']
     password = data['account'][account_index]['password']
-
+    
+    print(username)
+    print(password)
     
     try:
         session = cl.load_settings(f"./sessions/{username}.json")
@@ -26,7 +31,10 @@ def login_user(account_index):
             print('세션 정보를 활용해 로그인을 시도합니다.')
             try:
                 cl.set_settings(session)
+                
+                cl.set_device(random_device)
                 cl.login(username, password)
+                cl.dump_settings(f"./sessions/{username}.json")
     
                 try:
                     cl.get_timeline_feed()
@@ -36,14 +44,16 @@ def login_user(account_index):
                     old_session = cl.get_settings()
     
                     cl.set_settings({})
+                    cl.set_device(random_device)
                     cl.set_uuids(old_session["uuids"])
-    
+                    
                     cl.login(username, password)
                     cl.dump_settings(f"./sessions/{username}.json")
             except Exception as e:
                 print("세션 정보를 활용해 로그인에 실패했습니다.: %s" % e)
     except:
         print(f'🥔 {username} 로그인을 시도합니다.')
+        cl.set_device(random_device)
         cl.login(username, password)
         cl.dump_settings(f"./sessions/{username}.json")
         print(f'🥔 {username} 로그인 완료.')
@@ -51,23 +61,94 @@ def login_user(account_index):
     update_account_status(account_index, "running")
     return cl
 
+def generate_random_device():
+    devices = [
+        {
+            "model": "SM-G935F",
+            "manufacturer": "Samsung",
+            "android_version": 24,
+            "android_release": "7.0",
+            "resolution": "1440x2560",
+            "dpi": "560dpi"
+        },
+        {
+            "model": "SM-G920F",
+            "manufacturer": "Samsung",
+            "android_version": 22,
+            "android_release": "5.1.1",
+            "resolution": "1440x2560",
+            "dpi": "560dpi"
+        },
+        {
+            "model": "SM-N950F",
+            "manufacturer": "Samsung",
+            "android_version": 28,
+            "android_release": "9.0",
+            "resolution": "1440x2960",
+            "dpi": "560dpi"
+        },
+        {
+            "model": "SM-G975F",
+            "manufacturer": "Samsung",
+            "android_version": 29,
+            "android_release": "10.0",
+            "resolution": "1440x3040",
+            "dpi": "560dpi"
+        },
+        {
+            "model": "SM-G973F",
+            "manufacturer": "Samsung",
+            "android_version": 29,
+            "android_release": "10.0",
+            "resolution": "1440x3040",
+            "dpi": "560dpi"
+        },
+        {
+            "model": "SM-A505F",
+            "manufacturer": "Samsung",
+            "android_version": 28,
+            "android_release": "9.0",
+            "resolution": "1080x2340",
+            "dpi": "420dpi"
+        },
+        {
+            "model": "SM-A750F",
+            "manufacturer": "Samsung",
+            "android_version": 26,
+            "android_release": "8.0",
+            "resolution": "1080x2220",
+            "dpi": "420dpi"
+        },
+        {
+            "model": "SM-G610F",
+            "manufacturer": "Samsung",
+            "android_version": 23,
+            "android_release": "6.0.1",
+            "resolution": "1080x1920",
+            "dpi": "420dpi"
+        },
+        {
+            "model": "SM-A520F",
+            "manufacturer": "Samsung",
+            "android_version": 24,
+            "android_release": "7.0",
+            "resolution": "1080x1920",
+            "dpi": "420dpi"
+        },
+        {
+            "model": "SM-J710F",
+            "manufacturer": "Samsung",
+            "android_version": 23,
+            "android_release": "6.0.1",
+            "resolution": "720x1280",
+            "dpi": "320dpi"
+        }
+    ]
+    return random.choice(devices)
+
+
 
 def random_user_agent():
-    user_agents = [
-        "Mozilla/5.0 (Linux; Android 10; SM-N960F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/108.0.5359.128 Mobile Safari/537.36 Instagram 253.0.0.23.114 Android (29/10; 450dpi; 1080x2085; samsung; SM-N960F; crownlte; samsungexynos9810; hr_HR; 399993162)",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone14,2; iOS 16_3_1; en_GB; en-GB; scale=3.00; 1170x2532; 463736449) NW/3",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone12,5; iOS 16_3_1; it_IT; it-IT; scale=3.00; 1242x2688; 463736449)",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone14,2; iOS 16_4_1; it_IT; it; scale=3.00; 1170x2532; 463736449)",
-        "Mozilla/5.0 (Linux; Android 13; 2109119DG Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 Instagram 278.0.0.21.117 Android (33/13; 440dpi; 1080x2166; Xiaomi; 2109119DG; lisa; qcom; it_IT; 464315251)",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone12,1; iOS 16_3_1; es_US; es-US; scale=2.00; 828x1792; 463736449)",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone12,1; iOS 16_1_1; it_IT; it-IT; scale=2.21; 828x1792; 463736449)",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone14,4; iOS 16_3_1; en_IT; en-IT; scale=2.88; 1080x2338; 463736449) NW/3",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 275.1.0.17.100 (iPhone15,3; iOS 16_2; it_IT; it-IT; scale=3.00; 1290x2796; 458156275)",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 278.0.0.19.115 (iPhone12,1; iOS 16_4; it_IT; it-IT; scale=2.00; 828x1792; 463736449)",
-        "Mozilla/5.0 (Linux; Android 10; SM-A217M Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 Instagram 278.0.0.21.117 Android (29/10; 280dpi; 720x1448; samsung; SM-A217M; a21s; exynos850; pt_BR; 464315250)",
-        "Mozilla/5.0 (Linux; Android 13; 2107113SG Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 Instagram 278.0.0.21.117 Android (33/13; 440dpi; 1080x2186; Xiaomi; 2107113SG; vili; qcom; it_IT; 464315251)",
-        
-    ]
     return random.choice(user_agents)
 
 
@@ -160,14 +241,30 @@ def process_hashtags(client, account_index, account_list):
     request_count = 0
     thread_delay_count = 0
     global row_count
+    global hashtag_file
+    max_request_count = random.randint(30, 40)
+    
+    with open(f"./hashtags/hashtag목록_{hashtag_file}.csv", "r", encoding='utf-8') as csvfile: # 해시태그 읽어오기
+        csv_reader = csv.reader(csvfile)
+        hashtags = [row[0] for row in csv_reader]
+        num_lines = len(hashtags)
+
+    
     while True:
         with row_count_lock:
-            if row_count >= num_lines:
-                break
             hashtag = hashtags[row_count - 1]
             row_count += 1
 
         try:
+            if row_count == num_lines:
+                row_count = 0
+                num_lines = 0
+                hashtag_file += 1
+                with open(f"./hashtags/hashtag목록_{hashtag_file}.csv", "r", encoding='utf-8') as csvfile: # 해시태그 읽어오기
+                    csv_reader = csv.reader(csvfile)
+                    hashtags = [row[0] for row in csv_reader]
+                    num_lines = len(hashtags)
+    
             if thread_delay_count == 10: time.sleep(180)
             posts = search_posts_by_hashtag(client, hashtag)
             save_posts_to_csv(posts, hashtag, f"snsreview.csv")
@@ -180,7 +277,7 @@ def process_hashtags(client, account_index, account_list):
             request_count += 1  # 요청 횟수 증가
 
             # 요청 횟수가 40에 도달하면, 계정을 교체
-            if request_count >= 40:
+            if request_count >= max_request_count:
                 update_account_status(account_index, "ready")  # 현재 계정 상태를 'ready'로 변경
                 ready_accounts = [i for i, account in enumerate(account_list) if account["status"] == "ready"]  # 'ready'인 계정 목록을 가져옴
 
@@ -192,6 +289,7 @@ def process_hashtags(client, account_index, account_list):
                 account_index = random.choice(ready_accounts)  # 'ready'인 계정 중 하나를 선택
                 client = login_user(account_index)  # 새로운 계정으로 로그인합니다.
                 request_count = 0  # 요청 횟수를 초기화합니다.
+                max_request_count = random.randint(30, 40)
 
             
         except Exception as e:
@@ -232,14 +330,14 @@ if __name__ == "__main__":
     print('🌳 계정 리스트 로딩.')
     with open('account.json', 'r') as file:
         data = json.load(file)
-
+    print(f'{len(data)}개 로딩 완료.')
+    
+    print('🌳 User Agent 리스트 로딩.')  
+    with open('user-agent.json', 'r') as file:
+        user_agents = json.load(file)
+    print(f'{len(user_agents)}개 로딩 완료.')
+    
     available_accounts = [account for account in data['account'] if account["status"] != "ban"]
-
-
-    with open("hashtag.csv", "r", encoding='utf-8') as csvfile: # 해시태그 읽어오기
-        csv_reader = csv.reader(csvfile)
-        hashtags = [row[0] for row in csv_reader]
-        num_lines = len(hashtags)
 
     num_accounts = len(available_accounts)
 
